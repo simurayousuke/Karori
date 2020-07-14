dafb902514bb4ec308e89879d792a47b8566db46
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-14 16:54:02 +0900

    init

---
+++ src/main/java/cn/zhuangcloud/karori/common/HelloController.java
@@ -0,0 +1,8 @@
+package cn.zhuangcloud.karori.common;
+import com.jfinal.core.Controller;
+
+public class HelloController extends Controller {
+    public void index() {
+        renderText("Hello JFinal World.");
+    }
+}
