8f00322aaec3137e989453594551b81c70f09729
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-16 13:33:39 +0900

    update api(test)

--- src/main/java/cn/zhuangcloud/karori/common/api/v1/FoodApi.java
+++ src/main/java/cn/zhuangcloud/karori/common/api/v1/FoodApi.java
@@ -1,10 +1,15 @@
 package cn.zhuangcloud.karori.common.api.v1;
 
+import cn.zhuangcloud.karori.common.model.Composition;
+import cn.zhuangcloud.karori.common.model.Food;
+import com.jfinal.kit.Ret;
+
 public class FoodApi extends ApiV1 {
 
     public void upload() {
-
-        fail();
+        Composition composition = getBean(Composition.class);
+        Food food = getBean(Food.class);
+        renderJson(Ret.ok().set("food", food).set("composition", composition));
     }
 
 }
