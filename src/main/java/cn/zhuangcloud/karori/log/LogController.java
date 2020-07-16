20d8af3aa49d40b594b310746f645717f37c44bd
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-17 01:10:00 +0900

    i18b

--- src/main/java/cn/zhuangcloud/karori/log/LogController.java
+++ src/main/java/cn/zhuangcloud/karori/log/LogController.java
@@ -11,18 +11,40 @@ import com.jfinal.aop.Before;
     食物表-》联查
  */
 
+@Before(NeedLogin.class)
 public class LogController extends MyController {
 
     /*
     选择日期和三餐-》扫码-》如果存在则列出可选食物表，本人提交的优先-》选择量        -》提交
                          如果不存在则显示添加按钮，跳转到添加页面-》跳转回选择量
+        search barcode/name/mine -> fid
+        date,type,weight -> post
      */
 
-    @Before(NeedLogin.class)
     public void index() {
         title("foodLog");
         render("index.html");
     }
 
+    public void barcode() {
+        title("foodLog");
+        render("barcode.html");
+    }
+
+    public void foodname() {
+        title("foodLog");
+        render("foodname.html");
+    }
+
+    public void mine() {
+        title("foodLog");
+        render("mine.html");
+    }
+
+    public void post() {
+        //todo need fid para
+        title("foodLog");
+        render("post.html");
+    }
 
 }
