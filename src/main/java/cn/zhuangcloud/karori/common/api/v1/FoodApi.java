0555067ddbef73f947f4b58f6c46e69c96bdeee8
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-16 13:46:30 +0900

    add bo

--- src/main/java/cn/zhuangcloud/karori/common/api/v1/FoodApi.java
+++ src/main/java/cn/zhuangcloud/karori/common/api/v1/FoodApi.java
@@ -1,14 +1,14 @@
 package cn.zhuangcloud.karori.common.api.v1;
 
+import cn.zhuangcloud.karori.common.bo.FoodBO;
 import cn.zhuangcloud.karori.common.model.Composition;
-import cn.zhuangcloud.karori.common.model.Food;
 import com.jfinal.kit.Ret;
 
 public class FoodApi extends ApiV1 {
 
     public void upload() {
         Composition composition = getBean(Composition.class);
-        Food food = getBean(Food.class);
+        FoodBO food = getBean(FoodBO.class);
         renderJson(Ret.ok().set("food", food).set("composition", composition));
     }
 
