4eb1c7511c5ff06d1fe7c5b6abbb387982f2fa12
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-16 13:28:09 +0900

    update dao

--- src/main/java/cn/zhuangcloud/karori/common/model/_MappingKit.java
+++ src/main/java/cn/zhuangcloud/karori/common/model/_MappingKit.java
@@ -16,6 +16,8 @@ import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 public class _MappingKit {
 
     public static void mapping(ActiveRecordPlugin arp) {
+        arp.addMapping("t_composition", "cid", Composition.class);
+        arp.addMapping("t_food", "fid", Food.class);
         arp.addMapping("t_user", "uid", User.class);
     }
 }
