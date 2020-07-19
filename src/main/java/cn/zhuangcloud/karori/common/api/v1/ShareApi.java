48a5e0260f676119c14d41fd77f499e1569b2d28
庄庄庄庄庄 <yzz0427@vip.qq.com>
2020-07-19 15:37:06 +0900

    share

--- src/main/java/cn/zhuangcloud/karori/common/api/v1/ShareApi.java
+++ src/main/java/cn/zhuangcloud/karori/common/api/v1/ShareApi.java
@@ -3,9 +3,11 @@ package cn.zhuangcloud.karori.common.api.v1;
 import cn.zhuangcloud.karori.common.api.v1.validator.ShareApiValidator;
 import cn.zhuangcloud.karori.common.api.v1.validator.TokenDateRequired;
 import cn.zhuangcloud.karori.common.base.ApiV1;
+import cn.zhuangcloud.karori.common.interceptor.NeedLogin;
 import cn.zhuangcloud.karori.common.model.Share;
 import cn.zhuangcloud.karori.statistic.StatisticService;
 import com.jfinal.aop.Before;
+import com.jfinal.aop.Clear;
 import com.jfinal.aop.Inject;
 import com.jfinal.i18n.Res;
 import com.jfinal.kit.Ret;
@@ -17,6 +19,7 @@ public class ShareApi extends ApiV1 {
     @Inject
     StatisticService statisticService;
 
+    @Clear(NeedLogin.class)
     @Before(TokenDateRequired.class)
     public void dateAndType() {
         Date date = getAttr("__date");
